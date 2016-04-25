6641170919487ebcc221f8c7456ff987e43e6115
yangkuang <yangkuang@bupt.edu.cn>
2016-04-25 14:58:11 +0800

    newest changes

--- src/main/java/edu/bupt/sia/ccn/services/CCNServiceManager.java
+++ src/main/java/edu/bupt/sia/ccn/services/CCNServiceManager.java
@@ -1,10 +1,13 @@
 package edu.bupt.sia.ccn.services;
 
+import edu.bupt.sia.ccn.CCNServiceHandler;
 import edu.bupt.sia.osgi.OSGIContoller;
+import org.ccnx.ccn.CCNHandle;
 import org.ccnx.ccn.config.ConfigurationException;
 import org.ccnx.ccn.impl.support.Log;
 import org.ccnx.ccn.io.CCNFileInputStream;
 import org.ccnx.ccn.io.CCNInputStream;
+import org.ccnx.ccn.protocol.ContentName;
 import org.ccnx.ccn.protocol.MalformedContentNameStringException;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.Version;
@@ -28,7 +31,7 @@ public class CCNServiceManager{
         System.out.println("CCNServiceManager Start!");
     }
 
-    public boolean service_existed(String serviceName) {
+    public boolean service_existed(String serviceName) {//Check whether a specific service already existed in the serviceTable(or already be installed)
         boolean compare_result = false;
         if (_serviceTable.get(serviceName) != null) {
             return true;
@@ -44,13 +47,13 @@ public class CCNServiceManager{
         return compare_result;
     }
 
-    public boolean service_installed(String serviceName) { //serviceName == bundleSymbolicName
-        boolean default_result = false;
-        if (_serviceTable.get(serviceName) != null) {
-            return true;
-        }
-        return default_result;
-    }
+    //public boolean service_installed(String serviceName) { //serviceName == bundleSymbolicName
+    //    boolean default_result = false;
+    //    if (_serviceTable.get(serviceName) != null) {
+    //        return true;
+    //    }
+    //    return default_result;
+    //}
 
     public boolean serviceTable_withinSize() {
         boolean default_result = false;
@@ -66,11 +69,19 @@ public class CCNServiceManager{
         Log.info("CCN service is removed!", serviceName);
     }
 
-    public CCNFileInputStream fetchService(String serviceName) {
+    public CCNFileInputStream fetchService(String serviceName) throws IOException, MalformedContentNameStringException, ConfigurationException {
+        String ccnserviceName = "ccnx:/" + serviceName + ".jar";
+        CCNServiceHandler ccnserviceHandler = new CCNServiceHandler(ccnserviceName);
+        ContentName serviceContentName = ContentName.fromURI(ccnserviceName);
+        CCNHandle service_ccnHandle = ccnserviceHandler.getCCNHandle();
 
+        CCNFileInputStream serviceStream = new CCNFileInputStream(serviceContentName, service_ccnHandle);
+        //here this function is needed to be completed
+
+        return serviceStream;
     }
 
-    public void addService(String serviceName) {
+    public void addService(String serviceName) throws IOException, MalformedContentNameStringException, ConfigurationException {
         Bundle bundle = _serviceController.getBundleContext().getBundle(serviceName);
         long serviceID = bundle.getBundleId();
         Version serviceVersion = bundle.getVersion();
@@ -91,7 +102,7 @@ public class CCNServiceManager{
 
         _serviceTable.put(serviceName, CCNService_Object);
 
-        if (service_installed(serviceName)) {
+        if (service_existed(serviceName)) {
             Log.info("Start local ccn service: {0}", serviceName);
             startLocalService(serviceName);
         } else {
